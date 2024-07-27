30ff437a89c3483a36996213b83362e144242df6
Sushma <allurisushma1993@gmail.com>
2024-07-27 18:13:50 -0500

    [RUST] Update integer project warnings

--- Rust/intergers/src/main.rs
+++ Rust/intergers/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let number1 = 24;
-    let number2 = 42;
-    let minimum =
+    let number1: i32 = 24;
+    let number2: i32 = 42;
+    let minimum: i32 =
         if number1 < number2 {
             number1
         } else {
             number2
-            
+
         };
     println!("{}", minimum);
     if number1 > number2 {
@@ -16,10 +16,10 @@ fn main() {
         println!("{} <= {}", number1, number2);
     }
 
-    let mut a = 15;
-    let mut b = 40;
+    let mut a: i32 = 15;
+    let mut b: i32 = 40;
     while b != 0 {
-        let temp = b;
+        let temp: i32 = b;
         b = a % b;
         a = temp;
     }
